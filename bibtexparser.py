2aa0b1085b838c6e3f5f7db511918814a100c846
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2016-04-23 07:11:34 +0900

    Pass all test with quotes

--- bibtexparser.py
+++ bibtexparser.py
@@ -1,5 +1,88 @@
+import logging
+
+def getEntry(string):
+    """
+    Get a stripped reference entry as argument
+    Like 'author="Newton, Isaac"' for example.
+    """
+    print("Entry:", string, "EOS")
+    equal = string.find("=")
+    if equal == -1:
+        name = None
+        content = None
+    else:
+        first_dquote = string.find('"', equal)
+        name = string[0:equal].strip()
+        if first_dquote != -1:
+            # Found "", look for the next dquote
+            next_dquote = string.rfind('"')
+            content = string[first_dquote+1:next_dquote]
+        else:
+            # Did not found "", the user may be using {}
+            entry_substring = ""
+            raise "Not implemented"
+    return name, content
+
 class Reference:
     def __init__(self, string):
+        string = string.strip()
+        last_curly = string.rfind('}')
+        cursor = 0
+        if string[0] != "@":
+            logging.error("Did not found expected `@` at beginning of reference.")
+            self._error_handler()
+        else:
+            first_curly = string.find('{')
+            if first_curly == -1:
+                logging.error("Did not found expected `{` after reference type.")
+                self._error_handler()
+                return
+            self.type = string[1:first_curly].strip()
+            end_of_id = string.index(',', first_curly)
+            self.id = string[first_curly + 1:end_of_id].strip()
+            cursor = end_of_id
+            # Dealing with entries
+            # Loop ending condition: no "," found
+            self.entries = {}
+            has_entry = True
+            while has_entry:
+                first_dquote = string.find('"', cursor)
+                if first_dquote != -1:
+                    # Found "", look for the next dquote
+                    next_dquote = string.find('"', first_dquote + 1)
+                    next_comma = string.find(',', next_dquote + 1)
+                    has_entry = next_comma != -1
+                    if has_entry:
+                        entry_substring = string[cursor+1:next_comma].strip()
+                    else:
+                        entry_substring = string[cursor+1:last_curly].strip()
+                    cursor = next_comma + 1
+                else:
+                    # Did not found "", the user may be using {}
+                    curly_counter = 0
+                    first_curly_index = -1
+                    last_curly_index = -1
+                    while cursor < len(string) and (last_curly_index < 0 or first_curly_index < 0):
+                        if string[cursor] == "{":
+                            curly_counter += 1
+                            first_curly_index = cursor
+                        elif string[cursor] == "}":
+                            curly_counter -= 1
+                            if curly_counter == 0:
+                                last_curly_index = cursor
+                        cursor += 1
+                    next_comma = string.find(',', last_curly_index + 1)
+                    has_entry = next_comma != -1
+                    entry_substring = string[first_curly_index:last_curly_index+1].strip()
+                    cursor = next_comma + 1
+                # First entry may be empty if bibtex has an empty reference
+                if entry_substring == "":
+                    has_entry = False
+                else:
+                    name, content = getEntry(entry_substring)
+                    self.entries[name] = content
+
+    def _error_handler(self):
         self.type = None
         self.id = None
         self.entries = {}
--- test.py
+++ test.py
@@ -3,15 +3,60 @@ from bibtexparser import Reference
 
 class TestReferenceClass(unittest.TestCase):
 
-  def test_empty(self):
-      string = """
-      @{,
-      }
-      """
-      ref = Reference(string)
-      self.assertEqual(ref.type, '')
-      self.assertEqual(ref.id, '')
-      self.assertEqual(ref.entries, {})
+    def test_wrong_input_no_at(self):
+        string = "jg"
+        ref = Reference(string)
+        self.assertEqual(ref.type, None)
+        self.assertEqual(ref.id, None)
+        self.assertEqual(ref.entries, {})
+
+    def test_wrong_input_no_first_curly(self):
+        string = "@retrert grt r"
+        ref = Reference(string)
+        self.assertEqual(ref.type, None)
+        self.assertEqual(ref.id, None)
+        self.assertEqual(ref.entries, {})
+
+    def test_empty(self):
+        string = """
+
+        @{,
+        }
+
+        """
+        ref = Reference(string)
+        self.assertEqual(ref.type, '')
+        self.assertEqual(ref.id, '')
+        self.assertEqual(ref.entries, {})
+
+    def test_only_type(self):
+        string = """
+        @book {  newton1664gravity ,
+        }
+        """
+        ref = Reference(string)
+        self.assertEqual(ref.type, 'book')
+        self.assertEqual(ref.id, 'newton1664gravity')
+        self.assertEqual(ref.entries, {})
+
+    def test_single_entry_white_spaces(self):
+        string = """
+        @book{  newton1664gravity  ,
+            author  =
+            "Newton, Isaac"
+        }
+        """
+        ref = Reference(string)
+        self.assertEqual(ref.type, 'book')
+        self.assertEqual(ref.id, 'newton1664gravity')
+        self.assertEqual(ref.entries, {"author": "Newton, Isaac"})
+
+    def test_single_entry_no_white_spaces(self):
+        string = '@book{newton1664gravity,author="Newton, Isaac"}'
+        ref = Reference(string)
+        self.assertEqual(ref.type, 'book')
+        self.assertEqual(ref.id, 'newton1664gravity')
+        self.assertEqual(ref.entries, {"author": "Newton, Isaac"})
 
 if __name__ == '__main__':
     unittest.main()
